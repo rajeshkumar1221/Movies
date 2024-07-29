e46ba5e396b1796a2f4d8747898be9637f4b1dc1
Rajesh Karnatakam <rajeshkumar1221@gmail.com>
2024-07-29 21:41:52 +0530

    CreateMovie Code

---
+++ Movies.Application/Commands/Movies/CreateMovie/CreateMovieCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Movies.Application.Commands.Movies.CreateMovie
+{
+    public record CreateMovieCommand(string Title, string Description, string Category) : IRequest<int>;
+}
---
+++ Movies.Application/Commands/Movies/CreateMovie/CreateMovieCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Movies.Domain.Entities;
+using Movies.InfraStructure;
+
+namespace Movies.Application.Commands.Movies.CreateMovie
+{
+    public class CreateMovieCommandHandler : IRequestHandler<CreateMovieCommand, int>
+    {
+        private readonly MoviesDbContext _moviesDbContext;
+
+        public CreateMovieCommandHandler(MoviesDbContext moviesDbContext)
+        {
+            _moviesDbContext = moviesDbContext;
+        }
+
+        public async Task<int> Handle(CreateMovieCommand request, CancellationToken cancellationToken)
+        {
+            var movie = new Movie
+            {
+                Title = request.Title,
+                Description = request.Description,
+                Category = request.Category,
+                CreatedDate = DateTime.Now
+            };
+            await _moviesDbContext.AddAsync(movie, cancellationToken);
+
+            await _moviesDbContext.SaveChangesAsync(cancellationToken);
+            return movie.Id;
+        }
+    }
+}
